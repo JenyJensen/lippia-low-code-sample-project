33808d9b606a4bdab099608e0855f1f8dcc38d42
Jeny Jensen <jgbony@hotmail.com>
2025-12-22 12:42:05 -0300

    juntando TP anterior y TPFinal

--- src/main/java/ar/steps/AddNewTimeEntrySteps.java
+++ src/main/java/ar/steps/AddNewTimeEntrySteps.java
@@ -4,7 +4,7 @@ import ar.services.AddNewTimeEntryService;
 import io.cucumber.java.en.And;
 
 public class AddNewTimeEntrySteps {
-    @And("se crea una descripcion random para el time entry")
+    @And("a random description is generated and stored in a variable")
     public void createRandomDescriptionForTimeEntry(){
         AddNewTimeEntryService.createRandomDescription();
     }
